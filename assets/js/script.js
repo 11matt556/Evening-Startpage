6cf9b2fb31a5fea7a74cae8292ad31d8afa252b1
Matthew Howell <mrhowel@g.clemson.edu>
2020-11-23 21:50:49 -0500

    I did things

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,20 @@ function startTime() {
   var currentDate = new Date();
   var hr = parseInt(currentDate.getHours());
   var min = parseInt(currentDate.getMinutes());
+  var xM = "AM"
+
   //Add a zero in front of numbers<10
   if (min < 10) {
     min = "0" + min;
   }
-  document.getElementById("header-time").innerHTML = hr + ":" + min;
+
+  //Convert to 12 hour format
+  if (hr > 12) {
+    hr = hr - 12
+    xM = "PM"
+  }
+
+  document.getElementById("header-time").innerHTML = hr + ":" + min + " " + xM;
 
   var dateOptions = {
     weekday: "long",
@@ -27,7 +36,7 @@ function startTime() {
   var date = currentDate.toLocaleDateString("en-GB", dateOptions);
   document.getElementById("header-date").innerHTML = date;
 
-  var time = setTimeout(function(){ startTime() }, 60000);
+  var time = setTimeout(function () { startTime() }, 60000);
 }
 
 // You can add your own random quotes and stuff here
@@ -42,10 +51,10 @@ document.getElementById("header-quote").innerText = quotes[
 
 document.addEventListener('click', function (event) {
 
-	// If the clicked element doesn't have the right selector, bail
-	if (!event.target.matches('#link')) return;
-	// Otherwise, run your code...
-	document.body.style.opacity = 0;
+  // If the clicked element doesn't have the right selector, bail
+  if (!event.target.matches('#link')) return;
+  // Otherwise, run your code...
+  document.body.style.opacity = 0;
 
 }, false);
 
@@ -58,53 +67,53 @@ const $s = {
 
 // You can add your own search query here for anything you're interested in.
 // [command character]: ['search url', 'title']
-function engines () {
+function engines() {
   return {
-	g: ['https://google.com/search?q=', 'Google'],
-	i: ['https://google.com/search?tbm=isch&q=', 'Google Images'],
-	yt: ['https://youtube.com/results?search_query=', 'Youtube'],
-  s: ['https://stackoverflow.com/search?q=', 'Stackoverflow'],
-	a: ['https://web.archive.org/web/*/', 'Archive'],
-	w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia'],
+    g: ['https://google.com/search?q=', 'Google'],
+    i: ['https://google.com/search?tbm=isch&q=', 'Google Images'],
+    yt: ['https://youtube.com/results?search_query=', 'Youtube'],
+    s: ['https://stackoverflow.com/search?q=', 'Stackoverflow'],
+    a: ['https://web.archive.org/web/*/', 'Archive'],
+    w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia'],
   };
 }
 
-var search  = $s.qS('#search'),
-    input   = $s.qS('#search input[type="text"]'),
-    engines = engines();
+var search = $s.qS('#search'),
+  input = $s.qS('#search input[type="text"]'),
+  engines = engines();
 
 for (var key in engines)
   $s.qS('.search-engines').innerHTML += `<li><p title="${engines[key][1]}">!${key}</p></li>`;
 
 document.onkeypress = (e) => {
-    if (e.key == 's')
-      search.classList.add('active');
+  if (e.key == 's')
+    search.classList.add('active');
 
-    input.focus();
-    input.scrollIntoView();
+  input.focus();
+  input.scrollIntoView();
 
-    search.onkeyup = (e) => {
-      let args   = e.target.value.split(' '),
-          prefix = args[0],
-          engine = engines['g'][0], // the default engine (google in this case)
-          str    = 0;
+  search.onkeyup = (e) => {
+    let args = e.target.value.split(' '),
+      prefix = args[0],
+      engine = engines['g'][0], // the default engine (google in this case)
+      str = 0;
 
-      $s.qA('.search-engines li p').forEach((eng) => {
-        let current = eng.parentNode;
+    $s.qA('.search-engines li p').forEach((eng) => {
+      let current = eng.parentNode;
 
-        (prefix == eng.innerHTML)
-          ? current.classList.add('active')
-          : current.classList.remove('active');
-      });
+      (prefix == eng.innerHTML)
+        ? current.classList.add('active')
+        : current.classList.remove('active');
+    });
 
-      if (e.key == 'Enter') {
-        if (prefix.indexOf('!') == 0)
-          (engine = engines[prefix.substr(1)][0], str = 3);
+    if (e.key == 'Enter') {
+      if (prefix.indexOf('!') == 0)
+        (engine = engines[prefix.substr(1)][0], str = 3);
 
-        window.location = engine + args.join(' ').substr(str).toString().replace(/\s+/m, '%20');
-      } else if (e.keyCode == 27)
-        search.classList.remove('active');
-    };
+      window.location = engine + args.join(' ').substr(str).toString().replace(/\s+/m, '%20');
+    } else if (e.keyCode == 27)
+      search.classList.remove('active');
+  };
 };
 
 document.getElementById("container").addEventListener("DOMContentLoaded", startTime());
