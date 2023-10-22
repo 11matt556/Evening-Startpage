8e08e974b955ea188be3914dc184e6f515f22f6f
Matthew Howell <llmatt556@hotmail.com>
2023-10-22 13:16:26 -0400

    Update to new server path

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Clearing /root/nginx-proxy/html/hub"
-rm -rf /root/nginx-proxy/html/hub
-echo "Copying to /root/nginx-proxy/html/hub"
-rsync --recursive --info=progress2 * /root/nginx-proxy/html/hub
+echo "Clearing /root/nginx-server/html/hub"
+rm -rf /root/nginx-server/html/hub
+echo "Copying to /root/nginx-server/html/hub"
+rsync --recursive --info=progress2 * /root/nginx-server/html/hub
